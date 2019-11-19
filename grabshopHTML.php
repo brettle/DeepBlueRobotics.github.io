cc470d0da1e4e59ced5acbd746c60e8e85bbcd24
Someone with admin access to our GoDaddy account <deepbluerobotics@gmail.com>
2019-11-19 10:06:28 -0700

    Remove all php files not currently used

--- grabshopHTML.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $doc = new DomDocument;
-  // We need to validate our document before refering to the id
-  $doc->validateOnParse = true;
-  $doc->loadHtml(file_get_contents($_POST['url']));
-  echo $doc->getElementById($_POST['element'])->C14N();
-?>
